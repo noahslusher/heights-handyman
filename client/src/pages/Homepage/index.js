eef640d3b204bdac778602039b6085e60fab9b3a
Carl Wheezer <slushern42@gmail.com>
2022-09-13 11:26:49 -0600

    homepage styling started; logos added

--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -1,19 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import background from './background.jpg'
+import about from './about.svg'
+import contact from './contact.svg'
+import tools from './tools.svg'
 
 function Homepage() {
  return (
-  <div className="" id="">
-   <div className="text-xl font-bold underline" id="title">
+  <div className="bg-cover h-screen" id="" style={{backgroundImage: `url(${background})`}}>
+   <div className="flex justify-center py-10 text-3xl sm:text-6xl" id="title">
     Heights Handyman
    </div>
-   <div className="text-2xl" id="subtitle">
+   <div className="flex justify-center text-xl sm:text-2xl" id="subtitle">
     Your Home is One Step Closer to Realizing Its Full Potential
    </div>
-   <div className="" id="nav">
-    <div><Link to="/about">About</Link></div>
-    <div><Link to="/contact">Contact</Link></div>
-    <div><Link to="/services">Services</Link></div>
+   {/* Begin homepage nav bar */}
+   <div className="flex justify-center" id="nav">
+    
+    <div className="p-2">
+     <Link to="/about">
+      <div className="flex"><img src={about}/> About</div></Link></div>
+    
+    <div className="p-2">
+     <Link to="/contact">
+      <div className="flex"><img src={contact}/> Contact</div></Link></div>
+    
+    
+    <div className="p-2">
+     <Link to="/services">
+      <div className="flex"><img src={tools}/> Services</div></Link></div>
    </div>
   </div>
  )
